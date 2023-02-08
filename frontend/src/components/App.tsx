d52df5e90c2eac95bbb40bf0784207e932b08b70
roopeh <44685964+roopeh@users.noreply.github.com>
2023-02-08 16:22:00 +0200

    Frontend: Create basic structure

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "./AppBar";
+import "../styles/main.css";
+
+// eslint-disable-next-line arrow-body-style
+const App = () => {
+  return (
+    <div>
+      <AppBar />
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/AppBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/AppBar.css";
+
+const AppBar = () => (
+  <div className="appBar">
+    foo
+  </div>
+);
+
+export default AppBar;
